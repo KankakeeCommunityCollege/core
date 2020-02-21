e626de1dd6e6781e02ceeac6373dbb812d5c96e0
wdzajicek <tig.welder4@gmail.com>
2020-02-21 12:03:22 -0600

    Create slick sliders for Programs section...
    Add to slick configuration object:
    ```javascript
    .slick({
      // Other config
      rows: 3,
      slidesPerRow: 3,
    // More config
    }
    ```

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -2,6 +2,7 @@
 //import createVideoControls from './createVideoControls.js';
 import buildNewsFeed from './buildNewsFeed.js';
 import loadVideo from './loadVideo.js';
+import buildSliders from './sliders.js';
 //import './babelTest.js';
 
 loadVideo();  // Do NOT call inside a DOMContentLoaded function!
@@ -9,5 +10,6 @@ loadVideo();  // Do NOT call inside a DOMContentLoaded function!
 document.addEventListener('DOMContentLoaded', function() {
   //test();
   buildNewsFeed();
+  buildSliders();
   //createVideoControls();
 });
---
+++ assets/js/src/sliders.js
@@ -0,0 +1,16 @@
+
+function buildSliders() {
+  $('.slick-slider').slick({
+    dots: true,
+    rows: 3,
+    slidesPerRow: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
+    nextArrow:'<img alt="" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+  });
+}
+
+export default buildSliders;
