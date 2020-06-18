89543029bd33eac40f371cfd82a52c0d2485863c
wdzajicek <tig.welder4@gmail.com>
2020-06-18 14:56:32 -0500

    Add alerts to the homepage.

--- assets/js/src/sliders.js
+++ assets/js/src/sliders.js
@@ -1,15 +1,17 @@
 
 function buildSliders() {
-  $('.slick-slider').slick({
-    dots: true,
-    rows: 3,
-    slidesPerRow: 3,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
-    nextArrow:'<img alt="" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+  $(document).ready(function() {
+    $('.slick-slider').slick({
+      dots: true,
+      rows: 3,
+      slidesPerRow: 3,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow:'<img alt="" class="a-left control-c prev slick-prev" src="assets/img/dbl-prev.svg">',
+      nextArrow:'<img alt="" class="a-right control-c next slick-next" src="assets/img/dbl-next.svg">'
+    });
   });
 }
 
