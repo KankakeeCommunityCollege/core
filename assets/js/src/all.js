b23a77e66168a43472b92533a3b8d458a863fece
wdzajicek <tig.welder4@gmail.com>
2021-03-10 14:07:40 -0600

    Create JS to toggle search on 404 page.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,9 +1,11 @@
 import buildNewsFeed from './buildNewsFeed.js';
 import loadVideo from './loadVideo.js';
+import toggleSearch from './toggleSearch.js';
 //import './babelTest.js';
 
 loadVideo();  // Do NOT call inside a DOMContentLoaded function!
 
 document.addEventListener('DOMContentLoaded', function() {
   buildNewsFeed();
+  toggleSearch();
 });
